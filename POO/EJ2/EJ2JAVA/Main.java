c88c45ad290151b09509c2c1ab621f6a066ab811
Miikeel16 <mikel.garcia06@somo.eus>
2025-02-20 09:37:45 +0100

    aa

---
+++ POO/EJ2/EJ2JAVA/Main.java
@@ -0,0 +1,7 @@
+package POO.EJ2.EJ2JAVA;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(.toString);
+    }
+}
--- POO/EJ2/EJ2JAVA/Persona.java
+++ POO/EJ2/EJ2JAVA/Persona.java
@@ -4,21 +4,24 @@ public class Persona{
     private String nombre;
     private int edad;
     private String dni;
-    private char sexo;
+    private String sexo;
     private double peso;
     private double altura;
+    private static final int sobrePeso=1;
+    private static final int pesoIdeal=0;
+    private static final int pesoBajo=-1;
 
     //////CONSTRUCTOR POR DEFECTO//////
-    public Persona(char psexo){
-        this.nombre="";
-        this.edad=0;
+    public Persona(String psexo){
+        this.nombre="Mikel";
+        this.edad=18;
         this.dni=generaDNI();
-        this.sexo=comprobarSexo(psexo);
-        this.peso=0.0;
-        this.altura=0.0;
+        this.sexo="H";
+        this.peso=70;
+        this.altura=181;
     }
     /////CONSTRUCTOR RAPIDO/////////////
-    public Persona(String pnombre, char psexo, int pedad){
+    public Persona(String pnombre, String psexo, int pedad){
         this.nombre=pnombre;
         this.edad=pedad;
         this.dni=generaDNI();
@@ -27,7 +30,7 @@ public class Persona{
         this.altura=0.0;
     }
     /////CONSTRUCTOR CON TODOS LOS ATRIBUTOS//////
-    public Persona(String pnombre, int pedad, char psexo, double ppeso, double paltura){
+    public Persona(String pnombre, int pedad, String psexo, double ppeso, double paltura){
         this.nombre=pnombre;
         this.edad=pedad;
         this.dni=generaDNI();
@@ -109,16 +112,17 @@ public class Persona{
             if (letra==22) {
                 System.out.println("E");
             }
-        String dni=numero+""+letra;
-        return (dni);
+        String pdni=numero+""+letra;
+        return pdni;
     }
-    private char comprobarSexo(char psexo){
-        if (psexo == 'M' || psexo == 'H'){
+    private String comprobarSexo(String psexo){
+        if (psexo == "M" || psexo == "H" || psexo == "NB" ){
             return psexo;
         }else{
-            psexo='M';
+            psexo="M";
             return psexo;
         }
+    
     }
     public boolean esMayorDeEdad(int pedad){
         if (pedad>=18){
@@ -127,19 +131,16 @@ public class Persona{
             return false;
         }
     }
-    public int calcularIMC(double ppeso){
-        int sobrepeso=1;
-        int peso_ideal=0;
-        int peso_bajo=-1;
-        double imc = peso / (altura*altura);
-        if(altura<=0){
-            return sobrepeso;
+    public int calcularIMC(double ppeso, double paltura){
+        double imc = ppeso / (paltura*paltura);
+        if(paltura<=0){
+            return this.sobrePeso;
         }else  if (imc < 18) {
-            return peso_bajo;
+            return this.pesoBajo;
         }else if (imc < 25 ) {
-            return peso_ideal;
+            return this.pesoIdeal;
         }else{
-            return sobrepeso;
+            return this.sobrePeso;
         }
     }
     ////////////TOSTRING////////////////
@@ -163,10 +164,10 @@ public class Persona{
     public int getEdad(){
         return this.edad;
     }
-    public void setSexo(char psexo){
+    public void setSexo(String psexo){
         this.sexo=psexo;
     }
-    public char getSexo(){
+    public String getSexo(){
         return this.sexo;
     }
     public void setPeso(double ppeso){
@@ -181,6 +182,34 @@ public class Persona{
     public double getAltura(){
         return this.altura;
     }
+     public void setDni(String pdni){
+        this.dni=pdni;
+    }
+    public String getDni(){
+        return this.dni;
+    }
     ////////////////EQUALS//////////////
-    
+    public boolean equals(Persona p){
+        if (this.nombre != p.getNombre()){
+            return false;
+        }
+        else if (this.edad != p.getEdad()){
+            return false;
+        }
+        else if (this.sexo != p.getSexo()){
+            return false;
+        }
+        else if (this.peso != p.getPeso()){
+            return false;
+        }
+        else if (this.altura != p.getAltura()){
+            return false;
+        }
+        else if (this.dni != p.getDni()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }   
 }
