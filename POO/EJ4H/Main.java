6b79631eae10bef871c22ac5c114f99154cbb9e3
Miikeel16 <mikel.garcia06@somo.eus>
2025-03-05 12:26:12 +0100

    a

--- POO/EJ4H/Main.java
+++ POO/EJ4H/Main.java
@@ -1,11 +1,13 @@
 package POO.EJ4H;
 
 public class Main {
+    public static void main(String[] args) {
     Perro perro = new Perro();
     Gato gato = new Gato();
     Pato pato = new Pato();
 
     perro.hacerSonido();
     gato.hacerSonido();
-    Pato.hacerSonido();
+    pato.hacerSonido();
+    }
 }
