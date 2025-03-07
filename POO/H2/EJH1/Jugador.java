ce61722c09634ab152905ec3134484967bee494f
Miikeel16 <mikel.garcia06@somo.eus>
2025-03-07 11:11:24 +0100

    a

---
+++ POO/H2/EJH1/Jugador.java
@@ -0,0 +1,11 @@
+package POO.H2.EJH1;
+
+public abstract class Jugador {
+    private String Nombre;
+    private String Nacionalidad;
+    private String nombreEquipo;
+    private int torneosGanados;
+    private int puntosRanking;
+    private String categoria;
+}
+
